507bd15639d4e4050a2349478603ff35c6f07721
Adib Legastelois <adiblegastelois@MacBook-Air-de-Adib.local>
2021-02-16 23:49:51 +0100

    Using join function

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 class Pizza:
 
-    def __init__(self, name, price):
+    def __init__(self, name, price, ingredients):
         self.name = name
         self.price = price
-
+        self.ingredients = ingredients
 
     def show(self):
         print(f"PIZZA: {self.name} : {self.price} $")
+        print(", ".join(self.ingredients)) # Show All ingredients in Tuples properly
 
 
-pizza1 = Pizza("4 seasons", 10.50)
+pizza1 = Pizza("4 seasons", 10.50, ("mozza", "mushrooms", "spinach", "olive oil"))
 pizza1.show()
 
