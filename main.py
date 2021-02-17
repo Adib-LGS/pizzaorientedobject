e3c3445b85797fc3a1691bdcf5838ce406ca92b6
Adib Legastelois <adiblegastelois@MacBook-Air-de-Adib.local>
2021-02-17 23:21:15 +0100

    Add Veggy parameter

--- main.py
+++ main.py
@@ -1,15 +1,30 @@
 class Pizza:
 
-    def __init__(self, name, price, ingredients):
+    def __init__(self, name, price, ingredients, veggy=False):
         self.name = name
         self.price = price
         self.ingredients = ingredients
+        self.veggy = veggy
 
     def show(self):
-        print(f"PIZZA: {self.name} : {self.price} $")
-        print(", ".join(self.ingredients)) # Show All ingredients in Tuples properly
+        if self.veggy:
+            veg_str = " - Veggy"
+            print(f"PIZZA: {self.name} : {self.price} $ " + veg_str)
+        else:
+            print(f"PIZZA: {self.name} : {self.price} $")
+        print(", ".join(self.ingredients))  # Show All ingredients in Tuples properly
+        print()
 
 
-pizza1 = Pizza("4 seasons", 10.50, ("mozza", "mushrooms", "spinach", "olive oil"))
-pizza1.show()
+"""pizza1 = Pizza("4 seasons", 10.50, ("mozza", "mushrooms", "spinach", "olive oil"))
+pizza1.show()"""
 
+pizzas = (
+    Pizza("4 seasons", 10.50, ("mozza", "mushrooms", "spinach", "olive oil"), True),
+    Pizza("liberta", 11.50, ("tuna", "eggs", "onions", "tomatoes")),
+    Pizza("samoan", 09.50, ("chiken", "potatoes", "mustard", "salad")),
+)
+
+for i in pizzas:
+    if i.price > 10:
+        i.show()
